c78b11da528e0cecad17d8787affcfe76c9586cf
Ivan <49728539+KaminoontyoSenpai2000@users.noreply.github.com>
2019-10-23 21:12:39 +0300

    Add files via upload

---
+++ 14. Fourteenth Lesson class work 23.10.2019/main.js
@@ -0,0 +1,65 @@
+/*function add(a, b =67) {
+	return a + b;
+}
+
+const addTo2 = add.bild(underfined, 2);
+const addTo2 = add.bild(underfined, 2);
+
+console.log(addTo2(3));
+console.log(addTo3());
+*/
+
+
+
+/*class Text {
+	constructor() {
+		this.field = 10;
+	}
+
+	method1() {
+		return field + 20;
+	}
+
+	method2() {
+		return field + 40;
+	}
+
+	method3() {
+		return field + 60;
+	}
+}
+
+const t = new Test();
+
+const t_test_data = {
+	field:10,
+};
+
+function test_method1(my_this) {
+	return Test.method1(my_this);
+}
+
+function test_method2(my_this) {
+	return Test.method2(my_this);
+}
+
+function test_method3(my_this) {
+	return Test.method3(my_this);
+}
+
+test_method1.call(t_test_data);
+
+const t = new Test();
+
+function fcaller(f) {
+	const r = f();
+	console.log(r);
+}
+
+const l = () => t.method1();
+
+fcaller(l);
+//fcaller(() => t.method());*/
+
+
+
